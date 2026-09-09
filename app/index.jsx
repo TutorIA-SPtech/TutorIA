797ce36060ac6269fa8a3c021667193ae30a123e
Gusta-PA <gustavo.almeida@sptech.school>
2026-09-09 19:13:32 -0300

    Configurações iniciais para inicio de tela de introdução

---
+++ app/index.jsx
@@ -0,0 +1,17 @@
+import { View, Text, StyleSheet } from "react-native";
+import { Link } from "expo-router";
+
+export default function IntroductionScreen(){
+
+    return (
+        <View style={styles.container}>
+            <Text>TutorIA</Text>
+            <Text>A trilha que se ajusta ao que você já sabe</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }})
