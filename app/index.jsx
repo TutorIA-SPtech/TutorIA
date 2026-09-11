edcd64603316ea20621f095f74d9f9eb639ff402
Gusta-PA <gustavo.almeida@sptech.school>
2026-09-11 19:41:42 -0300

    Finalização da tela de introdução

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,31 @@
-import { View, Text, StyleSheet } from "react-native";
-import { Link } from "expo-router";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function IntroductionScreen(){
-
     return (
         <View style={styles.container}>
-            <Text>TutorIA</Text>
-            <Text>A trilha que se ajusta ao que você já sabe</Text>
+            <LinearGradient
+                colors={['rgba(60, 70, 110, 0.4)', 'transparent']}
+                start={{ x: 1, y: 0 }}
+                end={{ x: 0.2, y: 0.6 }}
+                style={[StyleSheet.absoluteFill, styles.topRightGradient]}
+                pointerEvents="none" 
+            />
+
+            <LinearGradient
+                colors={['rgba(70, 45, 45, 0.5)', 'transparent']}
+                start={{ x: 0, y: 1 }}
+                end={{ x: 0.8, y: 0.4 }}
+                style={[StyleSheet.absoluteFill, styles.bottomLeftGradient]}
+                pointerEvents="none"
+            />
+
+            
+            <View style={styles.content}>
+            <Image source={require('../assets/tuti.png')} style={{width: 100, height: 130}}/>
+                <Text style={styles.title}>TutorIA</Text>
+                <Text style={styles.subtitle}>A trilha que se ajusta ao que você já sabe.</Text>
+            </View>
         </View>
     )
 }
@@ -14,8 +33,31 @@ export default function IntroductionScreen(){
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#0A0D14',
   },
+  topRightGradient: {
+    height: '60%', 
+  },
+  bottomLeftGradient: {
+    top: '40%', 
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-start', 
+    zIndex: 1,
+    paddingHorizontal: 20,
+  },
+  title: {
+    color: '#FFFFFF',
+    fontSize: 50,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    width: '80%',
+    paddingTop: 10,
+    color: '#7f82b3',
+    fontSize: 16,
+    textAlign: 'left',
+  }
 })
